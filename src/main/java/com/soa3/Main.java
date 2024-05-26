fdc973e91e5bf9f70466115cdc347cb2c00c63cb
yisun2001 <56770811+yisun2001@users.noreply.github.com>
2024-05-26 13:50:19 +0200

    Update Main.java

--- src/main/java/com/soa3/Main.java
+++ src/main/java/com/soa3/Main.java
@@ -1,8 +1,41 @@
 package com.soa3;
 
+import com.soa3.Backlog.Backlog;
+import com.soa3.Backlog.BacklogItem;
+import com.soa3.Person.Person;
+import com.soa3.Person.ProductOwner;
+import com.soa3.Person.ScrumMaster;
+import com.soa3.Report.Report;
+import com.soa3.Report.ReportExport;
+import com.soa3.Report.ReportExportPdf;
+import com.soa3.Report.ReportExportPng;
+import com.soa3.Sprint.Sprint;
+import com.soa3.Sprint.SprintType;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         //
+        List<Person> developers = new ArrayList<>();
+
+        Person productOwner = new ProductOwner("Pieter");
+        Person scrumMaster = new ScrumMaster("Jansen");
+
+        Project project = new Project("Project",1, productOwner);
+
+        Backlog backlog = project.getBacklog();
+
+        backlog.add(new BacklogItem("Create backend"));
+        backlog.add(new BacklogItem("Create frontend"));
+        backlog.add(new BacklogItem("Create database"));
+
+        Sprint sprint = new Sprint(project,"Sprint 1", LocalDate.parse("2024-03-03"),LocalDate.parse("2024-03-30"), SprintType.RELEASE, developers, scrumMaster);
+
+        ReportExport reportExport = new ReportExportPdf();
+//        reportExport.export(sprint);
     }
 
 }
